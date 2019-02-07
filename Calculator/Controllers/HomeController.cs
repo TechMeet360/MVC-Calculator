0c485e3aefd2f81911a5e3e72ae184f437c7b0eb
Suhas Parameshwara <Suhas.Parameshwara@biztalk360.co.uk>
2019-02-07 21:11:21 +0530

    Add project files.

---
+++ Calculator/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+	using Models;
+
+	public class HomeController : Controller
+	{
+		// GET: Home
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Index(AdditionViewModel model, string command)
+		{
+			if (command == "add")
+			{
+				model.Result = model.A + model.B;
+			}
+
+			if (command == "sub")
+			{
+				model.Result = model.A - model.B;
+			}
+
+			if (command == "mul")
+			{
+				model.Result = model.A * model.B;
+			}
+
+			if (command == "div")
+			{
+				model.Result = model.A / model.B;
+			}
+
+			return View(model);
+		}
+	}
+}
---
+++ Calculator/Models/AdditionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+	public class AdditionViewModel
+	{  
+		public double A{ get; set; }
+		public double B{ get; set; }
+		public double Result {get; set; }
+	}
+}
